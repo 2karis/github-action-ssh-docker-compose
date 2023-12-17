49a1cbbed70e190516dc0c5f8de0403380c00457
Vaibhav <vaibhz@gmail.com>
2023-12-17 17:40:40 +0000

    Added option to prune

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,9 @@ fi
 if $DOCKER_COMPOSE_DOWN ; then
   remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/workspace\" ; } ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/workspace\" -xjv ; log 'Launching docker compose...' ; cd \"\$HOME/workspace\" ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" down"
 fi
+if $DOCKER_SYSTEM_PRUNE ; then
+  remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Bring down all containers...' ; [ -d $HOME/workspace ] && docker compose -f $HOME/workspace/docker-compose.yaml down ; log 'Removing workspace...'; rm -rf \"\$HOME/workspace\" ; } ; log 'Creating workspace directory...' ; docker system prune -af"
+fi
 
 ssh-add <(echo "$SSH_PRIVATE_KEY")
 
